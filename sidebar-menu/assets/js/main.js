45fc2fb17d83fc53e20f12e72ee40bf38ec42058
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-07-13 08:53:14 +0800

    active link

--- sidebar-menu/assets/js/main.js
+++ sidebar-menu/assets/js/main.js
@@ -13,3 +13,11 @@ const showSidebar = (toggleId, sidebarId) => {
 showSidebar("header-toggle", "sidebar");
 
 /*=============== LINK ACTIVE ===============*/
+const sidebarLink = document.querySelectorAll(".sidebar__link");
+
+function linkColor() {
+    sidebarLink.forEach((l) => l.classList.remove("active-link"));
+    this.classList.add("active-link");
+}
+
+sidebarLink.forEach((l) => l.addEventListener("click", linkColor));
