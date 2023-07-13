a4f948791f44e1cce68accc56d1a8ca6f505f2d3
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-07-13 08:41:21 +0800

    show sidebar

--- sidebar-menu/assets/js/main.js
+++ sidebar-menu/assets/js/main.js
@@ -1,4 +1,15 @@
 /*=============== SHOW SIDEBAR ===============*/
+const showSidebar = (toggleId, sidebarId) => {
+    const toggle = document.getElementById(toggleId),
+        sidebar = document.getElementById(sidebarId);
 
+    if (toggle && sidebar) {
+        toggle.addEventListener("click", () => {
+            sidebar.classList.toggle("show-sidebar");
+        });
+    }
+};
+
+showSidebar("header-toggle", "sidebar");
 
 /*=============== LINK ACTIVE ===============*/
