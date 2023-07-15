ff197bf6e70e511da19dc919e7b27c0f6325cd00
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-07-15 22:05:17 +0800

    hovereffect

--- Hero Sections/model/assets/js/main.js
+++ Hero Sections/model/assets/js/main.js
@@ -14,3 +14,12 @@ closeMenu.addEventListener("click", () => {
     navMenu.classList.toggle("hidden");
     navMenu.classList.remove("show");
 });
+
+new hoverEffect({
+    parent: document.querySelector(".home__img"),
+    intensity: 0.3,
+    image1: "assets/img/img1.jpg",
+    image2: "assets/img/img2.jpg",
+    displacementImage: "assets/img/diss.png",
+    imagesRatio: 450 / 300,
+});
