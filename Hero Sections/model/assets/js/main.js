c7f75e89615c73bd7fda69d5b461daf53e250489
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-07-15 22:05:56 +0800

    gsap animation

--- Hero Sections/model/assets/js/main.js
+++ Hero Sections/model/assets/js/main.js
@@ -23,3 +23,43 @@ new hoverEffect({
     displacementImage: "assets/img/diss.png",
     imagesRatio: 450 / 300,
 });
+
+/* NAV, TITLE, IMG */
+gsap.from(".nav", {
+    opacity: 0,
+    duration: 1,
+    delay: 1,
+    y: -30,
+});
+gsap.from(".home__title", {
+    opacity: 0,
+    duration: 1.2,
+    delay: 1.2,
+    y: -30,
+});
+gsap.from(".home__img", {
+    opacity: 0,
+    duration: 1.3,
+    delay: 1.3,
+    y: -50,
+});
+
+/*SOCIAL, SUBTITLE, BUTTON*/
+gsap.from(".home__social", {
+    opacity: 0,
+    duration: 1,
+    delay: 1.5,
+    y: 50,
+});
+gsap.from(".home__subtitle", {
+    opacity: 0,
+    duration: 1,
+    delay: 1.8,
+    y: 50,
+});
+gsap.from(".home__button", {
+    opacity: 0,
+    duration: 1,
+    delay: 2,
+    y: 50,
+});
