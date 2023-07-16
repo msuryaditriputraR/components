dadb7bebaff4f68e1a6dd81b839eaf03d4c6dca7
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-07-16 22:03:41 +0800

    add: menu show & hidden

---
+++ Hero Sections/coffee-3d/assets/js/main.js
@@ -0,0 +1,14 @@
+/*===== MENU SHOW & HIDDEN =====*/
+const navMenu = document.getElementById("nav-menu"),
+    toggleMenu = document.getElementById("nav-toggle"),
+    closeMenu = document.getElementById("nav-close");
+
+// SHOW
+toggleMenu.addEventListener("click", () => {
+    navMenu.classList.toggle("show");
+});
+
+// HIDDEN
+closeMenu.addEventListener("click", () => {
+    navMenu.classList.remove("show");
+});
