891ab3da782e583b3e466e704925d6de1698f53e
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-08-26 15:26:27 +0800

    add: gsap animation

--- Hero Sections/space/assets/js/main.js
+++ Hero Sections/space/assets/js/main.js
@@ -22,3 +22,11 @@ const linkAction = () =>
 navLink.forEach((nL) => nL.addEventListener("click", linkAction));
 
 /*=============== GSAP ANIMATION ===============*/
+gsap.from(".home__points", 1.5, { opacity: 0, y: -300, delay: 0.2 });
+gsap.from(".home__rocket", 1.5, { opacity: 0, y: 300, delay: 0.3 });
+gsap.from(".home__planet-1", 1.5, { opacity: 0, x: -200, delay: 0.8 });
+gsap.from(".home__planet-2", 1.5, { opacity: 0, x: 200, delay: 1 });
+gsap.from(".home__cloud-1", 1.5, { opacity: 0, y: 200, delay: 1.2 });
+gsap.from(".home__cloud-2", 1.5, { opacity: 0, x: 200, delay: 1.3 });
+gsap.from(".home__content", 1.5, { opacity: 0, y: -100, delay: 1.4 });
+gsap.from(".home__title img", 1.5, { opacity: 0, x: 100, delay: 1.6 });
