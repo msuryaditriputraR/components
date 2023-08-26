4692193169e14f78a4c01bbdb8a20ddba9e4dcc7
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-08-26 13:24:16 +0800

    add: show & hide menu

--- Hero Sections/space/assets/js/main.js
+++ Hero Sections/space/assets/js/main.js
@@ -1,8 +1,18 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+    navToggle = document.getElementById("nav-toggle"),
+    navClose = document.getElementById("nav-close");
 
+if (navToggle && navMenu && navClose) {
+    navToggle.addEventListener("click", () =>
+        navMenu.classList.add("show-menu")
+    );
+
+    navClose.addEventListener("click", () =>
+        navMenu.classList.remove("show-menu")
+    );
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
-
 /*=============== GSAP ANIMATION ===============*/
-
